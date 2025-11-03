2d645e1f27740a2a4484a62d344654b1b3fc5519
Luisfer Cordova <lcordova@paystand.com>
2025-11-03 15:12:26 -0700

    corregir controller create

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -314,7 +314,8 @@ export const getProductById = async (req, res) => {
     await newProduct.save();
 
     console.log(`Product ${id} saved to MongoDB.`);
-    if (id == 'ebf9738480d78e0132gnru') {
+    console.log('newProduct', newProduct);
+    if (newProduct.id == 'ebf9738480d78e0132gnru') {
       const flujos_codes = ["flowrate_speed_1", "flowrate_speed_2", "flowrate_total_1", "flowrate_total_2"];
       console.log('newProduct.status', newProduct.status);
       newProduct.status.map((stat) => {
