0d87b8a9b2974532417d6dc3a33dff950eb88242
Luisfer Cordova <lcordova@paystand.com>
2025-11-05 12:06:25 -0700

    correcciones flujo

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -113,14 +113,14 @@ export const getAllProducts = async (req, res) => {
           const flujos_codes = ["flowrate_speed_1", "flowrate_speed_2", "flowrate_total_1", "flowrate_total_2"];
           const flujos_total_codes = [ "flowrate_total_1", "flowrate_total_2"]
           if (product.id === 'ebf9738480d78e0132gnru' && flujos_codes.includes(stat.code)) {
-            stat.value = stat.value * 1.6;
+            stat.value = (stat.value * 1.6).toFixed(2);
             if (flujos_total_codes.includes(stat.code)) {
-              stat.value = stat.value / 10
+              stat.value = (stat.value / 10).toFixed(2);
             }
           }
           const arrayCodes = ["flowrate_speed_1", "flowrate_speed_2"];
           if (arrayCodes.includes(stat.code) && stat.value > 0) {
-              stat.value = stat.value / 10;
+              stat.value = (stat.value / 10).toFixed(2);
           }
           return stat;
       });
@@ -298,10 +298,10 @@ export const getProductById = async (req, res) => {
           const flujos_total_codes = [ "flowrate_total_1", "flowrate_total_2"]
           product.status.map((stat) => {
             if (flujos_codes.includes(stat.code)) {
-              stat.value = stat.value * 1.6;
+              stat.value = (stat.value * 1.6).toFixed(2);
             }
             if (flujos_total_codes.includes(stat.code)) {
-              stat.value = stat.value / 10
+              stat.value = (stat.value / 10).toFixed(2);
             }
             return stat;
           });
@@ -336,10 +336,10 @@ export const getProductById = async (req, res) => {
       const flujos_total_codes = [ "flowrate_total_1", "flowrate_total_2"]
       newProduct.status.map((stat) => {
         if (flujos_codes.includes(stat.code)) {
-          stat.value = stat.value * 1.6;
+          stat.value = (stat.value * 1.6).toFixed(2);
         }
         if (flujos_total_codes.includes(stat.code)) {
-          stat.value = stat.value / 10
+          stat.value = (stat.value / 10).toFixed(2);
         }
         return stat;
       });
