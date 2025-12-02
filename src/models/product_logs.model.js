222f0a77f39f6142ccdb93f1d80bedc228a7e1d1
Luisfer Cordova <lcordova@paystand.com>
2025-12-02 13:20:55 -0700

    corregir presion

--- src/models/product_logs.model.js
+++ src/models/product_logs.model.js
@@ -3,16 +3,16 @@ import mongoose from 'mongoose';
 const ProductLogSchema = new mongoose.Schema({
   producto: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
   product_id:{ type: String, required: true },
-  tds: { type: Number, required: true },
-  production_volume: { type: Number, required: true },  // en litros
-  rejected_volume: { type: Number, required: true },
-  temperature: { type: Number, required: true },
 
-  flujo_produccion: { type: Number, required: true },       // L/min actuales
-  flujo_rechazo: { type: Number, required: true },     // L/min actuales
-
-  tiempo_inicio: { type: Number, required: true },     // timestamp en segundos
-  tiempo_fin: { type: Number, required: true },        // timestamp en segundos
+  // TODOS OPCIONALES para aceptar logs de todos los tipos de productos
+  tds: { type: Number },
+  production_volume: { type: Number },  // en litros
+  rejected_volume: { type: Number },
+  temperature: { type: Number },
+  flujo_produccion: { type: Number },       // L/min actuales
+  flujo_rechazo: { type: Number },     // L/min actuales
+  tiempo_inicio: { type: Number },     // timestamp en segundos
+  tiempo_fin: { type: Number },        // timestamp en segundos
 
   source: { type: String, default: 'esp32' },          // opcional: fuente del log
   date: { type: Date, default: Date.now }              // fecha del evento
